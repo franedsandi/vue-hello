8a39602926a5f42ad6888c110f33083a47c3b0f6
franedsandi <franedsandi@gmail.com>
2023-09-27 14:33:49 +0200

    [feat] image and extend message

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,8 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-      message: "ciao, non ho capito niente della ultima mezza ora",,
+      message: "Ciao, non ho capito niente della ultima mezza ora, dovro riguardare quella parte della lezione",
+      image: "https://tse2.mm.bing.net/th?id=OIP.fgKVzajRF8NJTVpsBIC-CgHaEH&pid=Api&P=0&h=180",
     }
   }
 }) .mount("#app");
